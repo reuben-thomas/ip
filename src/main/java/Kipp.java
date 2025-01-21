2c7fe0fcf7886056cf3b53d366caa5fc7caf97af
Reuben Thomas <reubenthomas123@gmail.com>
2025-01-21 14:57:32 +0800

    Adding functionality to add and list to-do items
    Signed-off-by: Reuben Thomas <reubenthomas123@gmail.com>

--- src/main/java/Kipp.java
+++ src/main/java/Kipp.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Kipp {
     private static final String NAME = "KIPP";
     private static final String LOGO = """
@@ -7,6 +10,12 @@ public class Kipp {
             ██  ██  ██ ██      ██
             ██   ██ ██ ██      ██
             """;
+
+    private List<String> toDoList;
+
+    public Kipp() {
+        this.toDoList = new ArrayList<String>();
+    }
 
     public static String getName() {
         return Kipp.NAME;
@@ -27,8 +36,26 @@ public class Kipp {
     public String getResponse(String input) {
         if (input.equals("bye")) {
             return Kipp.getSignOut();
+        } else if (input.equals("list")) {
+            return this.getToDoListItems();
         } else {
-            return input;
+            return addToDoListItems(input);
         }
+    }
+
+    public String addToDoListItems(String item) {
+        this.toDoList.add(item);
+        return "added: " + item;
+    }
+
+    public String getToDoListItems() {
+        StringBuilder toDoListItems = new StringBuilder();
+        for (int i = 0; i < this.toDoList.size(); i++) {
+            toDoListItems.append((i + 1) + ". " + this.toDoList.get(i));
+            if (i != this.toDoList.size() - 1) {
+                toDoListItems.append("\n");
+            }
+        }
+        return toDoListItems.toString();
     }
 }
