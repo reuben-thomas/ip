4740a3bc6ef5eb9ad6f66a55d4c0257d2685b57e
Reuben Thomas <reubenthomas123@gmail.com>
2025-02-12 20:52:30 +0800

    Add JavaDoc comments for factory methods
    Signed-off-by: Reuben Thomas <reubenthomas123@gmail.com>

--- src/main/java/commandhandler/Command.java
+++ src/main/java/commandhandler/Command.java
@@ -4,13 +4,25 @@ package commandhandler;
  * Represents a command that can be executed by the user.
  */
 public final class Command {
+    /**
+     * String typed by user that will uniquely identify this command.
+     */
     private final String command;
+    /**
+     * Example usage containing both the command and sample arguments.
+     */
     private final String exampleUsage;
+    /**
+     * A brief description of what the command will do.
+     */
     private final String description;
+    /**
+     * The function that will be called to generate a response when the command is requested.
+     */
     private final CommandFunction commandFunction;
 
     /**
-     * Constructor for Command class.
+     * Constructor for Command class, used to create a new command without arguments.
      *
      * @param command         The command that will uniquely identify this command.
      * @param description     A brief description of what the command will do.
@@ -24,7 +36,7 @@ public final class Command {
     }
 
     /**
-     * Constructor for Command class.
+     * Constructor for Command class, used to create a new command with arguments.
      *
      * @param command         The command that will uniquely identify this command.
      * @param exampleArgs     Example arguments that the command can take.
@@ -44,6 +56,7 @@ public final class Command {
      * @param command         The command that will uniquely identify this command.
      * @param description     A brief description of what the command will do.
      * @param commandFunction The function that will be called when the command is executed.
+     * @return The new command object.
      */
     public static Command createCommandWithoutArgs(String command, String description,
                                                    CommandFunction commandFunction) {
@@ -57,20 +70,36 @@ public final class Command {
      * @param exampleArgs     Example arguments that the command can take.
      * @param description     A brief description of what the command will do.
      * @param commandFunction The function that will be called when the command is executed.
+     * @return The new command object.
      */
     public static Command createCommandWithArgs(String command, String exampleArgs,
                                                 String description, CommandFunction commandFunction) {
         return new Command(command, exampleArgs, description, commandFunction);
     }
 
+    /**
+     * Gets the command string used to identify the command.
+     *
+     * @return The command string.
+     */
     public String getCommand() {
         return this.command;
     }
 
+    /**
+     * Gets the description of what the command should do.
+     *
+     * @return The description of the command.
+     */
     public String getDescription() {
         return this.description;
     }
 
+    /**
+     * Gets the example usage of the command including the command and example arguments.
+     *
+     * @return The example usage of the command.
+     */
     public String getExampleUsage() {
         return this.exampleUsage;
     }
